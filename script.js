62b841bee074f22ca7221945a4a7f05f9c150206
robdor80 <ROB.DOR.80@GMAIL.COM>
2025-10-31 12:26:34 +0100

    runas

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btnGoogle').addEventListener('click', () => {
+  alert('🪶 Login con Google próximamente...');
+});
+
+document.getElementById('btnAcceso').addEventListener('click', () => {
+  alert('🔓 Acceso al Santuario concedido.');
+});
